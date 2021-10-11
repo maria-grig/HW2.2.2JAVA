8ec2a502fbc82fc59588cc3ff1d3b6c58cf6aa43
maria-grig <masha.grigorenko.89@gmail.com>
2021-10-12 00:05:46 +0300

    origin

---
+++ src/BmiService.java
@@ -0,0 +1,12 @@
+public class BmiService {
+public static double calucate(double index) {
+    private static double power(int weight, int i) {
+
+    int weight = 57;
+    int height = 163;
+
+    double index = power(weight, 2) / height;
+    }
+   return (index);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 57;
+        int height = 163;
+        double index = BmiService.calculate(index);
+
+        System.out.println(index);
+
+            }
+
+        }
+
+
